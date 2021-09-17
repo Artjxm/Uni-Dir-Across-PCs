03136df71b05ad37e8da65df235cfb6354590a63
Shad0n <tom31245@gmail.com>
2021-09-17 11:15:36 +0300

    5th semester

---
+++ 3rd-Grade/Fifth-Semester/AKSP-Java/src/pr1/Ping_pong_game.java
@@ -0,0 +1,11 @@
+package pr1;
+
+public class Ping_pong_game implements Runnable {
+
+    @Override
+    public void run() {
+        while(true) {
+            System.out.println();
+        }
+    }
+}
